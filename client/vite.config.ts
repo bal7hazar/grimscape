b484134765db62764d70b2516c542c15ab93ef1e
bal7hazar <bal7hazar@proton.me>
2024-09-26 14:07:41 -0400

    🔥  Remove PWA

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from "vite";
 import wasm from "vite-plugin-wasm";
 import react from "@vitejs/plugin-react";
 import topLevelAwait from "vite-plugin-top-level-await";
-import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,56 +10,6 @@ export default defineConfig({
     react(),
     wasm(),
     topLevelAwait(),
-    VitePWA({
-      registerType: "autoUpdate",
-      devOptions: {
-        enabled: true,
-      },
-      includeAssets: [
-        "/assets/favicon.ico",
-        "/assets/apple-touch-icon.png",
-        "/assets/masked-icon.svg",
-      ],
-      manifest: {
-        name: "Grimscape",
-        short_name: "Grimscape",
-        description: "Solo dungeon crawler game.",
-        theme_color: "#000000",
-        icons: [],
-      },
-      workbox: {
-        runtimeCaching: [
-          {
-            urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
-            handler: "CacheFirst",
-            options: {
-              cacheName: "google-fonts-cache",
-              expiration: {
-                maxEntries: 10,
-                maxAgeSeconds: 60 * 60 * 24 * 365, // <== 365 days
-              },
-              cacheableResponse: {
-                statuses: [0, 200],
-              },
-            },
-          },
-          {
-            urlPattern: /^https:\/\/fonts\.gstatic\.com\/.*/i,
-            handler: "CacheFirst",
-            options: {
-              cacheName: "gstatic-fonts-cache",
-              expiration: {
-                maxEntries: 10,
-                maxAgeSeconds: 60 * 60 * 24 * 365, // <== 365 days
-              },
-              cacheableResponse: {
-                statuses: [0, 200],
-              },
-            },
-          },
-        ],
-      },
-    }),
   ],
   build: {
     outDir: "dist",
